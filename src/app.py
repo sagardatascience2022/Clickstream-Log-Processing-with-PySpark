6a331726a9102e40e8e06a67306775a04ef466cc
sagarm2022 <103205009+sagardatascience2022@users.noreply.github.com>
2025-10-29 11:51:46 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ Enhanced Streamlit app for interactive clickstream analysis with additional feat
 """
 import streamlit as st
 import pandas as pd
+import plotly
 import plotly.express as px
 import plotly.graph_objects as go
 from pyspark.sql import SparkSession
@@ -267,4 +268,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
